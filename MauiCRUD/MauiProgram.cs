2597de10f77aa1d89c56e58a7d502fcb4c6c30fb
igorbdaluz <igorbdaluz@hotmail.com>
2024-07-02 23:49:25 -0300

    Adicionando controles da DevExpress no projeto MAUI

--- MauiCRUD/MauiProgram.cs
+++ MauiCRUD/MauiProgram.cs
@@ -2,32 +2,31 @@
 using Microsoft.Extensions.Logging;
 using DevExpress.Maui;
 
-namespace MauiCRUD
+namespace MauiCRUD;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .UseDevExpress()
-                .UseDevExpressCollectionView()
-                .UseDevExpressControls()
-                .UseDevExpressEditors()
-                .UseDevExpressDataGrid()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseDevExpress(false)
+            .UseDevExpressCollectionView()
+            .UseDevExpressControls()
+            .UseDevExpressEditors()
+            .UseDevExpressDataGrid()
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+		builder.Logging.AddDebug();
 #endif
-            Routing.RegisterRoute("endereco", typeof(frmEndereco));
+        Routing.RegisterRoute("endereco", typeof(frmEndereco));
 
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
